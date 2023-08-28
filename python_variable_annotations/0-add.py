939fa6e08132346610d6e94cf5745711a719aed7
Martin <martinleiro9@gmail.com>
2023-08-28 07:05:05 -0700

    0 1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-""" type-annotated function add that takes a
-    float a and a float b as arguments and
-    returns their sum as a float"""
+"""type-annotated function"""
 
 
 def add(a: float, b: float) -> float:
+    """return add"""
     return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-""" Write a type-annotated function concat that
-    takes a string str1 and a string str2 as arguments
-    and returns a concatenated string"""
+"""type-annotated function"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """return concatenation"""
     return str1 + str2
