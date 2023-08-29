fb21d20c36ab5c07841baa0343070fd3cc07be8f
Martin <martinleiro9@gmail.com>
2023-08-29 16:55:36 -0700

    task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays in ascending order"""
+    completed_task_lst = []
+
+    for _ in range(n):
+        completed_task_lst.append(await task_wait_random(max_delay))
+
+    return sorted(completed_task_lst)
