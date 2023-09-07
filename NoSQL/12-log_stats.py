8f693356530c939bc3e5bfd4e6864831e84a5caf
Martin <martinleiro9@gmail.com>
2023-09-07 10:31:31 -0700

    log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,19 +4,21 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient("mongodb://localhost:27017/")
-    db = client["logs"]
-    collection = db["nginx"]
+    # db = client["logs"]
+    # collection = db["nginx"]
+    collection = client.logs.nginx
 
     lst_method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    dic_method_count = {}
+
     # create a dictionary with method and the count of methods
+    dic_method_count = {}
     for method in lst_method:
         dic_method_count[method] = collection.count_documents({"method": method})
 
-    # Count the number of documents with method=GET and path=/status
+    # count the number of documents with method=GET and path=/status
     status_count = collection.count_documents({"method": "GET", "path": "/status"})
 
-    # Show method counts
+    # show
     print("Methods:")
     for k, v in dic_method_count.items():
         print(f"\tmethod {k}: {v}")
