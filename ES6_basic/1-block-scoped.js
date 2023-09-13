95f913ac08ec6407dccca1dfc1d257f150c91226
Hentype85 <martinleiro9@gmail.com>
2023-09-13 16:21:49 -0700

    1 block scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+  var task = false;
+  var task2 = true;
 
-    if (trueOrFalse) {
-      task = false;
-      task2 = true;
-    }
-
-    return [task, task2];
+  if (trueOrFalse) {
+    task = false;
+    task2 = true;
   }
+
+  return [task, task2];
+}
