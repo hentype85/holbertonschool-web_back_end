3617cf9ca508d7195e657bf556fe6da64ff76574
Hentype85 <martinleiro9@gmail.com>
2023-09-13 17:15:57 -0700

    arrow syntax function add

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -7,18 +7,4 @@ export default function getNeighborhoodsList() {
         return self.sanFranciscoNeighborhoods;
     };
 }
-
 //  ES6’s arrow syntax of the function add
-/*
-// syntax of the function add
-
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
-  };
-}
-*/
