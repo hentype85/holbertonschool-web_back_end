389c1a034f9a56137186cc97d1c4057b5887cade
Hentype85 <martinleiro9@gmail.com>
2023-09-13 17:14:39 -0700

    arrow syntax function add

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,15 +1,17 @@
-//  ES6’s arrow syntax of the function add
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = newNeighborhood => {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  }
 
+    const self = this;
+    this.addNeighborhood = (newNeighborhood) => {
+        self.sanFranciscoNeighborhoods.push(newNeighborhood);
+        return self.sanFranciscoNeighborhoods;
+    };
+}
+
+//  ES6’s arrow syntax of the function add
 /*
+// syntax of the function add
+
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
