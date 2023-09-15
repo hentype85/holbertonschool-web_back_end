60a7345e6a0c487e599c8eade41469edd379cf15
Hentype85 <martinleiro9@gmail.com>
2023-09-15 06:42:09 -0700

    course class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,12 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if(typeof name !== 'string') {
+    if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
     }
-    if(typeof length !== 'number') {
+    if (typeof length !== 'number') {
       throw TypeError('Length must be a number');
     }
-    if(!Array.isArray(students)) {
+    if (!Array.isArray(students)) {
       throw TypeError('Students must be an array');
     }
 
@@ -19,32 +19,36 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
 
   // setters
   set name(newName) {
-    if(typeof newName === 'string') {
+    if (typeof newName === 'string') {
         this._name = newName;
     }
     else {
         throw TypeError('Name must be a string')
     }
   }
+
   set length(newLength) {
-    if(typeof newLength === 'number') {
+    if (typeof newLength === 'number') {
         this._length = newLength;
     }
     else {
         throw TypeError('Length must be a number')
     }
   }
+  
   set students(newStudents) {
-    if(Array.isArray(newStudents)) {
+    if (Array.isArray(newStudents)) {
         this._students = newStudents;
     }
     else {
