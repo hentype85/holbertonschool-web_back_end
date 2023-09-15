77dbbede48d2f448c13b6e3360d65788b45f94b0
Hentype85 <martinleiro9@gmail.com>
2023-09-15 06:36:15 -0700

    course class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,36 +1,54 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students;
+  constructor(name, length, students) {
+    if(typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if(typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if(!Array.isArray(students)) {
+      throw TypeError('Students must be an array');
     }
 
-    // getters
-    get name() {
-        return this._name;
-    }
-    get length() {
-        return this._length;
-    }
-    get students() {
-        return this._students;
-    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    // setters
-    set name(newName) {
-        if (typeof newName === 'string') {
-            this._name = newName;
-        }
+  // getters
+  get name() {
+    return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  get students() {
+    return this._students;
+  }
+
+  // setters
+  set name(newName) {
+    if(typeof newName === 'string') {
+        this._name = newName;
     }
-    set length(newLength) {
-        if (typeof newLength === 'number') {
-            this._length = newLength;
-        }
+    else {
+        throw TypeError('Name must be a string')
     }
-    set students(newStudents) {
-        if (Array.isArray(newStudents)) {
-            this._students = newStudents;
-        }
+  }
+  set length(newLength) {
+    if(typeof newLength === 'number') {
+        this._length = newLength;
     }
+    else {
+        throw TypeError('Length must be a number')
+    }
+  }
+  set students(newStudents) {
+    if(Array.isArray(newStudents)) {
+        this._students = newStudents;
+    }
+    else {
+        throw TypeError('Students must be an array')
+    }
+  }
 }
-
