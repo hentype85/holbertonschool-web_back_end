dd3daa2ede8314a433d0b253055051359a28f0c7
Hentype85 <martinleiro9@gmail.com>
2023-09-15 07:33:55 -0700

    currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name){
+    this._code = code;
+    this._name = name;
+  }
+
+  // getter
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // setter
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
