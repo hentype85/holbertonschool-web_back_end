fdd8694670dec883c9c6c3ce180f2a4510e5d8dd
Hentype85 <martinleiro9@gmail.com>
2023-09-16 10:10:26 -0700

    main 7

--- ES6_classes/7-main.js
+++ ES6_classes/7-main.js
@@ -3,3 +3,10 @@ import Airport from "./7-airport.js";
 const airportSF = new Airport('San Francisco Airport', 'SFO');
 console.log(airportSF);
 console.log(airportSF.toString());
+/*
+const airportSF = new Airport();
+airportSF._name = 'San Francisco Airport';
+airportSF._code = 'SFO';
+console.log(airportSF);
+console.log(airportSF.toString());
+*/
