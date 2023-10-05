dcac71bcd7189f9bb5fdd7653ca00d701cd3e9dc
martin <martinleiro9@gmail.com>
2023-10-05 08:54:38 -0700

    fix2 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,13 +5,13 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const user = await signUpUser(firstName, lastName)
     .then((data) => ({
       status: 'fulfilled',
-      value: data
+      value: data,
     }));
 
   const photo = await uploadPhoto(fileName)
     .catch((data) => ({
       status: 'rejected',
-      value: data.toString()
+      value: data.toString(),
     }));
 
   return Promise.resolve([user, photo]);
