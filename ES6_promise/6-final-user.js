93f0889fc46dedc06b7403899a21c78615abbc87
martin <martinleiro9@gmail.com>
2023-10-05 08:55:56 -0700

    fix 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,17 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const user = await signUpUser(firstName, lastName)
     .then((data) => ({
       status: 'fulfilled',
-      value: data,
+      value: data
     }));
 
   const photo = await uploadPhoto(fileName)
     .catch((data) => ({
       status: 'rejected',
-      value: data.toString(),
+      value: data.toString()
     }));
 
   return Promise.resolve([user, photo]);
