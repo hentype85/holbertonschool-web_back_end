a576c9e5cdd33004f8a195816d6de64d7b12f67f
martin <martinleiro9@gmail.com>
2023-10-05 07:55:47 -0700

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,21 +1,16 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup (firstName, lastName, fileName) {
+export default async function handleProfileSignup (firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.all([userPromise, photoPromise])
-    .then((data) => {
-      ((data) => ({
-        status: 'OK',
-        value: result
-      }));
-    })
-    .catch((data) => {
-      ((data) => ({
-        status: 'rejected',
-        value: err
-      }));
-    });
+  const results = await Promise.allSettled([userPromise, photoPromise]);
+
+  const profileResults = results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason
+  }));
+
+  return profileResults;
 }
