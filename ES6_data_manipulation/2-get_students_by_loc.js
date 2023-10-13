62b5bbb410a0f830f744c1c29f69b5f953882786
martin <martinleiro9@gmail.com>
2023-10-13 00:53:00 +0000

    2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-export default function getStudentsByLocation (students, city) {
+export default function getStudentsByLocation(students, city) {
   // check students is an array
   if (Object.getPrototypeOf(students) === Array.prototype) {
     return students.filter((student) => student.location === city);
