dcafbb463f6785424dabc0173483964a565ebb06
martin <martinleiro9@gmail.com>
2023-10-13 01:27:16 +0000

    4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,9 +7,9 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     return [];
   }
 
-  return students.filter(student => student.location === city).map(student => {
+  return students.filter((student) => student.location === city).map((student) => {
 
-    const [newGrade] = newGrades.filter(grade => grade.studentId === student.id);
+    const [newGrade] = newGrades.filter((grade) => grade.studentId === student.id);
     return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
   });
 }
